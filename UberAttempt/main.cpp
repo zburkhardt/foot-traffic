fcc576968ecd923b7e113edee003ed46e1ffd905
Zach Burkhardt <zburkhardt18@gmail.com>
2019-10-05 22:54:54 -0400

    Worked on parsing CSV data file

--- UberAttempt/main.cpp
+++ UberAttempt/main.cpp
@@ -2,9 +2,26 @@
 #include <fstream>
 
 using namespace std;
-ifstream inFile;
+
+void parseFile();
+
+int roomOneOcc = 0, roomTwoOcc = 0;
 
 int main()
+{
+    bool jump = false, emergency = false;
+
+    parseFile();
+
+    cout << "Number of people in Room A: " << roomOneOcc << endl;
+    cout << "Number of people in Room B: " << roomTwoOcc << endl;
+    cout << "----------------" << endl;
+    cout << "Total number of people in the building: " << (roomOneOcc + roomTwoOcc) << endl;
+
+	return 0;
+}
+
+void parseFile()
 {
     ifstream fout;
     fout.open("rfidData.csv");
@@ -17,18 +34,30 @@ int main()
 
     else
     {
-        cout << "Reading in file data:"
-        string id_tag = "", name = "", room = "", time = "";
+        cout << "Reading in file data: ";
+
+        string id_tag = "", name = "", room = "", time = "", entry = "";
 
         // Read in to variables from csv RFID data file
         while (!(getline(fout,id_tag,',').eof())) {
             getline(fout,name,',');
             getline(fout,room,',');
-            getline(fout,time,'\n');
+            getline(fout,time,',');
+            getline(fout,entry,'\n');
+
+            if (room == "1")
+                if (entry == "1")
+                    ++roomOneOcc;
+                else
+                    --roomOneOcc;
+            else if (room == "2")
+                if (entry == "1")
+                    ++roomTwoOcc;
+                else
+                    --roomTwoOcc;
         }
+
+        fout.close();
+        cout << "COMPLETE" << endl << endl;
     }
-
-	return 0;
 }
-
-
